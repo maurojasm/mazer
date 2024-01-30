8537e2e4b2bd99385d598d382ca1001febf6cbd0
Mauricio Rojas <79242002+maurojasm@users.noreply.github.com>
2024-01-30 13:28:12 -0800

    added initial assets and header files

---
+++ mazer.cpp
@@ -0,0 +1,11 @@
+/*
+    @author: Mauricio Rojas
+    @details: Mazer game dev
+*/
+
+#include <iostream>
+
+int main() {
+
+    return 0;
+}
---
+++ src/dot.h
@@ -0,0 +1,16 @@
+// #ifdef DOT_PLAYER
+// #define DOT_PLAYER
+
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+// #include <SDL.h>
+// #include <SDL_image.h>
+#include <stdio.h>
+#include <string>
+#include <fstream>
+
+class Dot {
+
+};
+
+// #endif
