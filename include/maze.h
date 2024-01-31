6edd3f260c53505631fbc72081ae33337ce736df
Mauricio Rojas <79242002+maurojasm@users.noreply.github.com>
2024-01-31 10:02:30 -0800

    added necessary header files

---
+++ include/maze.h
@@ -0,0 +1,11 @@
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+// #include <SDL.h>
+// #include <SDL_image.h>
+#include <stdio.h>
+#include <string>
+#include <fstream>
+
+class Maze {
+
+};
