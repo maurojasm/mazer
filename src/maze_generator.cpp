a93b2857f995be31687c50e4cf1716ea521b622f
Mauricio Rojas <79242002+maurojasm@users.noreply.github.com>
2024-04-03 10:55:01 -0700

    impl arbitrary set mazes

--- src/maze_generator.cpp
+++ src/maze_generator.cpp
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include <string>
+#include <ctime>
 
 #include "../include/maze_generator.h"
 
 Maze_Gen::Maze_Gen() {
-
+    std::srand(std::time(0));
 }
 
 string Maze_Gen::generate_maze(int size) {
-    string maze = "89c895575576a35dea836aa2a";
+    int index = 0, m_size = 0;
+    
+    if (size == 5) {
+        m_size = easy_collection.size();
+    } else if(size == 8) {
+        m_size = mid_collection.size();
+    } else {
+        m_size = hard_collection.size();
+    }
+    // for some reason calling rand once gets 5 all the time
+    for(int i = 0; i < 3; i++) {
+        index = rand()%m_size;
+        // printf("%d\n", index);
+    }
+    string maze = easy_collection[index];
+
     return maze;
 }
