05dc351af80f0c2bda3c319bdfa408260499bdbf
Mauricio Rojas <79242002+maurojasm@users.noreply.github.com>
2024-04-03 14:40:56 -0700

    fixed and cleaned maze generation

--- src/maze_generator.cpp
+++ src/maze_generator.cpp
@@ -8,22 +8,31 @@ Maze_Gen::Maze_Gen() {
     std::srand(std::time(0));
 }
 
-string Maze_Gen::generate_maze(int size) {
-    int index = 0, m_size = 0;
-    
-    if (size == 5) {
-        m_size = easy_collection.size();
-    } else if(size == 8) {
-        m_size = mid_collection.size();
-    } else {
-        m_size = hard_collection.size();
-    }
+int get_rand(int n) {
+    int x = 0;
     // for some reason calling rand once gets 5 all the time
     for(int i = 0; i < 3; i++) {
-        index = rand()%m_size;
-        // printf("%d\n", index);
+        x = rand()%n;
     }
-    string maze = easy_collection[index];
+    return x;
+}
+
+string Maze_Gen::generate_maze(int size) {
+    string maze = "";
+    
+    switch (size) {
+    case 5:
+        maze = easy_collection[get_rand(easy_collection.size())];
+        break;
+    case 8:
+        maze = mid_collection[get_rand(mid_collection.size())];
+        break;
+    case 10:
+        maze = hard_collection[get_rand(hard_collection.size())];
+        break;
+    default:
+        break;
+    }
 
     return maze;
 }
