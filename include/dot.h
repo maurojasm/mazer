2ff1b1506f46185201bd40e3009229114220349e
Mauricio Rojas <79242002+maurojasm@users.noreply.github.com>
2024-04-23 11:25:00 -0700

    add check_win func and fix formatting

--- include/dot.h
+++ include/dot.h
@@ -11,40 +11,42 @@
 #include "tile.h"
 
 class Dot {
-    public:
-        // constructor
-        Dot();
+public:
+    // constructor
+    Dot();
 
-        // render dot to the screen
-        void render(Texture &dot_texture, SDL_Rect& camera, SDL_Renderer *renderer);
+    // render dot to the screen
+    void render(Texture& dot_texture, SDL_Rect& camera, SDL_Renderer* renderer);
 
-        // handle key presses to adjust velocity
-        void handle_event(SDL_Event& e);
+    // handle key presses to adjust velocity
+    void handle_event(SDL_Event& e);
 
-        // move the dot
-        void move(int LEVEL_HEIGHT, int LEVEL_WIDTH, int TOTAL_TILES, std::vector<Tile*> game_tiles);
+    // move the dot
+    void move(int LEVEL_HEIGHT, int LEVEL_WIDTH, int TOTAL_TILES, std::vector<Tile*> game_tiles);
 
-        // set camera over the dot
-        void set_camera(SDL_Rect& camera, int SCREEN_WIDTH, 
-                        int SCREEN_HEIGHT, int LEVEL_WIDTH, 
-                        int LEVEL_HEIGHT);
+    // set camera over the dot
+    void set_camera(SDL_Rect& camera, int SCREEN_WIDTH,
+        int SCREEN_HEIGHT, int LEVEL_WIDTH,
+        int LEVEL_HEIGHT);
 
-        // checks collision between dot and wall tile
-        bool touches_wall(SDL_Rect hit_box, std::vector<Tile*> game_tiles, int TOTAL_TILES);
+    // checks collision between dot and wall tile
+    bool touches_wall(SDL_Rect hit_box, std::vector<Tile*> game_tiles, int TOTAL_TILES);
 
-    private:
-        // dot dimensions (hit box)
-        static const int d_height = 20;
-        static const int d_width = 20;
+    bool check_win(int LEVEL_WIDTH, int LEVEL_HEIGHT);
 
-        // max dot velocity
-        static const int max_vel = 10;
+private:
+    // dot dimensions (hit box)
+    static const int d_height = 20;
+    static const int d_width = 20;
 
-        // dot's collision box
-        SDL_Rect hit_box;
+    // max dot velocity
+    static const int max_vel = 8;
 
-        // velocity of the dot
-        int x_vel, y_vel;
+    // dot's collision box
+    SDL_Rect hit_box;
+
+    // velocity of the dot
+    int x_vel, y_vel;
 };
 
 #endif
