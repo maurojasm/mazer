ff4a74ebded64541fb3574991921a52cbf0f9c7b
Mauricio Rojas <79242002+maurojasm@users.noreply.github.com>
2024-06-27 14:51:05 -0700

    refactor code and added comments

--- include/bullet.h
+++ include/bullet.h
@@ -14,18 +14,64 @@ using std::vector;
 
 class Bullet {
 public:
+    /*
+    Class constructor, creates bullet with given location, direction, and size.
+
+    @param x    x-coordinate hitbox's location
+    @param y    y-coordinate hitbox's location
+    @param dir  Direction variable of bullet
+    @param w    width of bullet's hitbox
+    @param h    height of bullet's hitbox
+    */
     Bullet(int x, int y, Direction dir, int w = 10, int h = 10);
+
+    /*
+    Checks for collision between bullet's hitbox, game tiles, and level dimensions.
+
+    @param level_w      level's width to check for out of bounds conditions
+    @param level_h      level's height to check for out of bounds conditions
+    @param game_tiles   vector of tile objects to check for collision
+    @return             true if collision occurs, false otherwise
+    */
     bool collision(int level_w, int level_h, vector<Tile*> game_tiles);
+
+    /*
+    Renders bullet based on passed texture, renderer, and relative to camera.
+
+    @param texture  Texture object of Bullet object to render
+    @param renderer renderer used to render texture
+    @param camera   camera to render in relation to
+    */
     void render(Texture* texture, SDL_Renderer* renderer, SDL_Rect& camera);
-    void move(int screen_w, int screen_h);
+
+    /*
+    Moves bullet and checks for collisions.
+
+    @param level_w      level's width to check for out of bounds conditions
+    @param level_h      level's height to check for out of bounds conditions
+    */
+    void move(int level_w, int level_h);
+
+    /*
+    Checks if bullet collisions with a wall tile.
+
+    @param game_tiles vector of tile objects to check for collision
+    */
     bool touches_wall(vector<Tile*> game_tiles);
 private:
-    // int b_x, b_y;
+    // bullet size
     int b_width, b_height;
+
+    // bullet velocity
     int b_vel, vel_x, vel_y;
+
+    // damage of bullet, @todo implement damage
     // int damage;
+
+    // bullet hitbox
     SDL_Rect hit_box;
 
+    // bullet direction
     Direction b_dir;
 };
 
--- src/bullet.cpp
+++ src/bullet.cpp
@@ -23,7 +23,9 @@ bool Bullet::collision(int screen_w, int screen_h, vector<Tile*> game_tiles) {
 
 bool Bullet::touches_wall(vector<Tile*> game_tiles) {
     for (unsigned i = 0; i < game_tiles.size(); i++) {
+        // from all tiles, check only the ones that are wall type
         if (game_tiles[i]->get_type() == 1) {
+            // check if collision occurs
             if (check_collision(hit_box, game_tiles[i]->get_hit_box())) {
                 return true;
             }
@@ -33,11 +35,14 @@ bool Bullet::touches_wall(vector<Tile*> game_tiles) {
 }
 
 void Bullet::render(Texture* texture, SDL_Renderer* renderer, SDL_Rect& camera) {
+    // quad to render to, location relative to camera
     SDL_Rect renderQuad{ (hit_box.x - camera.x), (hit_box.y - camera.y), b_width, b_height };
+    // render texture
     texture->render((hit_box.x - camera.x), (hit_box.y - camera.y), renderer, NULL, &renderQuad);
 }
 
 void Bullet::move(int screen_w, int screen_h) {
+    // move bullet based on direction given
     switch (b_dir) {
     case UP_DIR:
         hit_box.y -= b_vel;
